51df6d5a69d2f3fdcb61a904f3918c8b87e134ff
Jo3-h <joe.hosking1@gmail.com>
2024-07-11 11:25:45 +0930

    updated methods for finding hamiltonian path and assembling genome from best path found

--- 6. Genome Assembly Programming Challenge/Programming Assignment 1/Phi-X174 Error Free Reads.py
+++ 6. Genome Assembly Programming Challenge/Programming Assignment 1/Phi-X174 Error Free Reads.py
@@ -6,28 +6,41 @@ import sys
 # suffix/prefix match between two nodes. Fucnction to find hamiltonian paths to construct genome candidate.
 class overlap_graph:
 
-    def __init__(self, reads, error_prone=False, tests=False) -> None:
+    def __init__(self, reads, k=12, error_prone=False, tests=False) -> None:
         self.reads = reads
         self.error_prone = error_prone
-        self.candidate_pairs = self.__find_candidate_pairs(reads)
+        self.candidate_pairs = self.__find_candidate_pairs(reads, k)
         self.overlaps = self.__find_overlaps()
+        self.hamiltonian_path = []
         self.best_weight = 0
         self.tests = tests
         return
+    
+    # printing format for class including details of current state of best paths, candidate pairs, and overlaps
+    def __str__(self):
+        output = ''
+        output += '--- OVERLAP GRAPH ---\n\n(candidate pair) - overlap\n'
+        for key in self.candidate_pairs:
 
-        # for each read in the reads input, add a node and connect to all existing nodes
-        # with directional link equal to the longest prefix/sufffix match from 1 -> 2
-        for read in reads:
+            output += f'{self.reads[key[0]], self.reads[key[1]]} '
+            if key in self.overlaps:
+                output += str(self.overlaps[key])
+            output += '\n'
 
-            # add read node to 
-            self.add_node(read)
+        output += '\nBest Path\n'
+        for element in self.hamiltonian_path:
+            output += str(element) +', '
+
+        return output
+
 
     # private function to calculate the largest suffix/prefix overlap between to input reads
     def __compute_overlap(self, read1, read2):
 
         max_length = 0
-        for i in range(1, len(read1)):
-            suffix = read1[i:] # suffix equals last 'i' characters of read1
+        min_length = min(len(read1), len(read2))
+        for i in range(1, min_length + 1):
+            suffix = read1[-i:] # suffix equals last 'i' characters of read1
             prefix = read2[:i] # prefix equals first 'i' characters of read2
 
             # check whether suffix/prefix match, if new longest then update max_length
@@ -43,13 +56,12 @@ class overlap_graph:
         overlaps = {}
         for i, j in self.candidate_pairs:
             overlap_size = self.__compute_overlap(self.reads[i], self.reads[j])
-            if overlap_size > 0:
-                overlaps[(i, j)] = overlap_size
+            overlaps[(i, j)] = overlap_size
 
         return overlaps
 
     # private function to return set of pairs of candidate read indexes from the input reads list
-    def __find_candidate_pairs(self, reads, k=12):
+    def __find_candidate_pairs(self, reads, k):
 
         # initialise k-mer dict
         kmer_dict = {}
@@ -82,21 +94,76 @@ class overlap_graph:
 
     # private function to find hamiltonian path through graph
     def __find_hamiltonian_path(self):
-        path = []
-        visited = set()
         
+        # begin the path at the first index of reads
+        path = [self.reads[0]]
+        visited = set()
+        visited.add(self.reads[0])
+        best_path = []
+        pq = []
+
+        # add start node to pq
+        neighbours = [(start, end, self.overlaps[(start, end)]) for start, end in self.candidate_pairs if start == 0]
+        for neighbour in neighbours:
+            heapq.heappush(pq, (-neighbour[2], neighbour[1], path.copy(), visited.copy()))
+
+        while pq:
+
+            current_overlap, current_index, path, visited = heapq.heappop(pq)
+
+            neighbours = [(start, end, self.overlaps[(start,end)]) for start, end in self.candidate_pairs if start == current_index and end not in path]
+            neighbours = sorted(neighbours, key=lambda x: x[2])
+
+            # check continue conditions
+            if current_index in path or current_index in visited:
+                continue
+
+            # add current to path and visited
+            path.append(current_index)
+            visited.add(current_index)
+
+            # add unvisited neighbouring nodes to the pq
+            for neighbour in neighbours:
+                heapq.heappush(pq, (current_overlap-neighbour[2], neighbour[1], path.copy(), visited.copy()))
+
+            # check for path containing all indexes
+            if len(visited) == len(self.reads):
+
+                # add overlap from last index in current path back to first index
+                total_overlap = current_overlap - self.overlaps.get((current_index, 0), 0)
+                
+                # if total overlap is the best found path through graph update best path / overlap member variables
+                if total_overlap < self.best_weight:
+                    self.best_weight = total_overlap
+                    best_path = path
+        
+        return best_path
 
     # private function to assemble genome from the computed hamiiltonian path previously constructed
     def __assemble_genome(self):
-        genome = ''
-        return genome
+        
+        # for each index in best path, append non-overlap section to the genome
+        candidate_genome = self.reads[0]
+        previous_index = 0
+        for index in self.hamiltonian_path[1:]:
+
+            # get overlap from previous read index to current
+            current_overlap = self.overlaps.get((previous_index, index), 0)
+
+            # add non-overlapping section to candidate genome
+            candidate_genome += self.reads[index][current_overlap:]
+
+            previous_index = index
+
+        # remove matching prefix / suffix from candidate genome since reads[0] appears at start and end of path
+        candidate_genome = candidate_genome[:-self.overlaps.get((previous_index, 0), 0)]
+
+        return candidate_genome
 
     def compute_candidate_genome(self):
-
         
         self.hamiltonian_path = self.__find_hamiltonian_path()
         self.candidate_genome = self.__assemble_genome()
-        
         return
 
     def print_genome(self):
@@ -117,105 +184,12 @@ def main():
     reads = parse_input(test=True)
 
     # construct an overlap graph from reads
-    graph = overlap_graph(reads, error_prone=False)
+    graph = overlap_graph(reads, error_prone=False, k=12)
     graph.compute_candidate_genome()
+    #print(graph)
     graph.print_genome()
 
     return
 
 if __name__ == '__main__':
     main()
-
-'''
-    def add_node(self, read):
-
-        # if node is not already in nodes then add
-        if read not in self.nodes.keys():
-            self.nodes[read] = {}
-            for dest in self.reads:
-                if dest == read:
-                    continue
-                self.add_link(read, dest)
-
-        return
-    
-    def add_link(self, source: str, dest: str) -> None:
-
-        index = len(dest)
-        link = 0
-        # loop through values of start index until a vaild suffix/prefix match is found or start index equals length of source
-        while index > 0:
-            if source[-index:] == dest[:index]:
-                link = index
-                break
-            else:
-                index -= 1
-        
-        if link >= 0:
-            if source not in self.nodes:
-                self.nodes[source] = {}
-            self.nodes[source][dest] = link
-
-        return
-
-    def find_hamiltonian(self):
-
-        # create visited set to keep track of nodes already included in path
-        visited = set()
-        start_node = list(self.nodes.keys())[0]
-        current_node = str(start_node)
-        visited.add(current_node)
-        path = [current_node]
-        pq = []
-
-        # add neighbours to the priority queue and negate link since pq will give smallest values first
-        for neighbour, link in self.nodes[current_node].items():
-            heapq.heappush(pq, (-link, current_node, neighbour, visited, path))
-
-        # while there are nodes in the queue still, investigate queued nodes
-        while pq:
-
-            if self.tests:
-                for item in pq:
-                    print(item)
-                print(20*'-', end='\n\n')
-            
-            current_cost, current_node, dest, visited, path = heapq.heappop(pq)
-
-            if dest in visited:
-                continue
-            
-            visited.add(dest)
-            path.append(dest)
-
-            for neighbour, link in self.nodes[dest].items():
-                if neighbour not in visited:
-                    heapq.heappush(pq, (current_cost-link, dest, neighbour, visited.copy(), path.copy()))
-
-            # check whether all nodes have been visited, if so, add link from end node back to start node
-            if len(self.nodes) == len(visited):
-                total_weight = current_cost-self.nodes[dest][start_node]
-                if total_weight < self.best_weight:
-                    self.best_weight = total_weight
-                    path.append(start_node)
-                    self.best_path = path
-        
-        self.candidate_genome = self.compress(self.best_path, start_node)
-        return
-    
-    # function to recompress reads on hamiltonian path to a genome candidate
-    def compress(self, path, start_node):
-
-        # if path is empty return
-        if len(path) == 0:
-            return False
-
-        genome = path[0]
-        for i in range(1, len(path)):
-            current_read = path[i]
-            prev_read = path[i-1]
-            overlap_size = self.nodes[prev_read][current_read]
-            genome += current_read[overlap_size:]
-
-        return genome[:-len(start_node)]
-'''
